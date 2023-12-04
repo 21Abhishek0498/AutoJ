4d62f9afc93c879f24b0b79c4dc1774409a76f9f
vivek yadav <viv.yad89@gmail.com>
2023-12-05 00:09:12 +0530

    Transformer changes and fixed the code to parse and create TestClassBuilder object.

---
+++ src/main/java/com/auto/gen/junit/autoj/transformer/Tansformer.java
@@ -0,0 +1,45 @@
+package com.auto.gen.junit.autoj.transformer;
+
+import com.auto.gen.junit.autoj.dto.ClazzDependencies;
+import com.auto.gen.junit.autoj.dto.Method;
+import com.auto.gen.junit.autoj.dto.TestClassBuilder;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.FieldAccessExpr;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.SimpleName;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.Statement;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class Tansformer implements TransformerProcessor {
+    /**
+     * @param testClassBuilder
+     */
+    @Override
+    public void transform(TestClassBuilder testClassBuilder) {
+      List<Method> methods = testClassBuilder.getMethodList();
+      NodeList<Statement> statementList;
+      for(Method method : methods){
+        Optional<BlockStmt> statements = method.getMethodBody().getMethodBody();
+
+        if(statements.isPresent());
+          statementList = statements.get().getStatements();
+          for(Statement stmt : statementList){
+              Expression expression = stmt.asExpressionStmt().getExpression();
+              if(expression.isMethodCallExpr()){
+                  SimpleName className = expression.asMethodCallExpr().getName();
+                  NameExpr calledMethod = expression.asMethodCallExpr().asFieldAccessExpr().asNameExpr();
+                  List<ClazzDependencies> dependenciesMockList = testClassBuilder.getDependencies().stream()
+                          .filter( fields -> fields.getType().equals(className.getClass()))
+                          .collect(Collectors.toList());
+              }
+          }
+      }
+    }
+}
