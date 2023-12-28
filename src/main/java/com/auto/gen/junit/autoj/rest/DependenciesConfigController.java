87e93bde16c69253c39107e95cce289b72d74f5a
21Abhishek0498 <abhishekgorakh2104@gmail.com>
2023-12-28 20:16:14 +0530

    Adding Rest Controller

--- src/main/java/com/auto/gen/junit/autoj/rest/DependenciesConfigController.java
+++ src/main/java/com/auto/gen/junit/autoj/rest/DependenciesConfigController.java
@@ -1,16 +1,44 @@
 package com.auto.gen.junit.autoj.rest;
 
+import com.auto.gen.junit.autoj.service.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-
-
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-//@RestController
-//@RequestMapping(value = "/dependency")
+@RestController
+@RequestMapping(value = "/dependency")
 public class DependenciesConfigController {
 
+    @Autowired
+    private ServiceImpl serviceImpl;
+
+    @GetMapping(value = "/pom-path")
+    public ResponseEntity<Map<String, String>> getDependencies(@RequestParam(name = "pathToPom", required = true) String pathToPom) {
+        try {
+            return new ResponseEntity<>(serviceImpl.getJavaVersionAndSpringVersion(pathToPom), HttpStatus.OK);
+        } catch (Exception e) {
+            Map<String, String> error = new HashMap<>();
+            error.put("Error processing POM file: ", e.getMessage());
+            return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/check-and-add")
+    public ResponseEntity<String> checkAndAddDependency(@RequestParam(name = "pathToPom", required = true) String pathToPom) {
+        try {
+            return new ResponseEntity<>( serviceImpl.checkAndAdd(pathToPom), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error processing request: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
 
 
